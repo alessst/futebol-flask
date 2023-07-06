085e68d6ef057ec57742cd75fd84565fdb2c36b8
alessandrotome <alessandro100-100@hotmail.com>
2023-07-06 20:55:53 -0300

    novos times

--- app.py
+++ app.py
@@ -1,29 +1,26 @@
 from flask import Flask, render_template, request
-import urllib.request, json
+import csv
 import random
 
-url = 'https://api.api-futebol.com.br/v1/campeonatos/14/tabela'
-headers = {'Authorization': 'Bearer test_af647478dcd44baa8bc59073498bf7'}
 
 app = Flask(__name__)
+times_data = []
+jdata = []
+
+with open('times.csv', 'r') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        jdata.append(row)
+
 def obter_dados_time(n):
-    req = urllib.request.Request(url, headers=headers)
-    response = urllib.request.urlopen(req)
-    data = response.read()
-    jdata = json.loads(data)
     dados_time = jdata[n]
-    time = dados_time["time"]
-    nome = time["nome_popular"]
-    escudo_time = time["escudo"]
+    nome = dados_time["nome_popular"]
+    escudo_time = dados_time["escudo"]
     return nome, escudo_time
 
 @app.route("/", methods=['GET', 'POST'])
 def tabela():
-    numero = random.randint(0, 19)
-    if request.method == 'POST':
-        valor = request.form.get('valor')
-       
-
+    numero = random.randint(0, 39)
     nome_time, escudo = obter_dados_time(numero)
     print(nome_time)
     return render_template("tabela.html", escudo=escudo, nome_time=nome_time)
