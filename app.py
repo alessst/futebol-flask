dfc0b3a01d571b0ae7e867cdb704022302fb584b
alessandrotome <alessandro100-100@hotmail.com>
2023-07-12 14:32:59 -0300

    commit pontos

--- app.py
+++ app.py
@@ -1,30 +1,24 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
 import csv
 import random
 
-
-lista_pontos = []
 app = Flask(__name__)
-jdata = []
-
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pontuacao.sqlite3'
-
 db = SQLAlchemy(app)
 
-class pontos(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	nome = db.Column(db.String(50))
-	pt = db.Column(db.String(10))
+class Pontuacao(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(50))
+    pt = db.Column(db.String(10))
 
-	def __init__(self, nome, pt):
-		self.nome = nome
-		self.pt = pt
-                
+    def __init__(self, nome, pt):
+        self.nome = nome
+        self.pt = pt
+
 with open('times.csv', 'r') as csvfile:
     reader = csv.DictReader(csvfile)
-    for row in reader:
-        jdata.append(row)
+    jdata = list(reader)
 
 def obter_dados_time(n):
     dados_time = jdata[n]
@@ -32,25 +26,25 @@ def obter_dados_time(n):
     escudo_time = dados_time["escudo"]
     return nome, escudo_time
 
+@app.route("/obter_nova_imagem")
+def obter_nova_imagem():
+    numero = random.randint(0, 122)
+    nome, escudo = obter_dados_time(numero)
+    return jsonify({"nome": nome, "escudo": escudo})
+
 @app.route("/", methods=['GET', 'POST'])
 def tabela():
     numero = random.randint(0, 122)
     nome_time, escudo = obter_dados_time(numero)
-    print(nome_time)
-    return render_template("tabela.html", escudo=escudo, nome_time=nome_time, pontos=pontos.query.all())
 
-@app.route("/add_pontos", methods=['GET', 'POST'])
-def jogador():
-    nome = request.form.get("nome")
-    ponto = request.form.get("ponto")
     if request.method == "POST":
-        jogador = pontos(nome, ponto)
+        nome = request.form.get("nome")
+        ponto = request.form.get("ponto")
+        jogador = Pontuacao(nome, ponto)
         db.session.add(jogador)
         db.session.commit()
-        return redirect(url_for("tabela"))
-    return render_template("jogador.html")
 
-
+    return render_template("tabela.html", escudo=escudo, nome_time=nome_time, jogadores=Pontuacao.query.all())
 
 if __name__ == "__main__":
     with app.app_context():
